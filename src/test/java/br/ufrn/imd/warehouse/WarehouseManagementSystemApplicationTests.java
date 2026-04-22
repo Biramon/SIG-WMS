8503433f155bd9459f604e2e1e749348c63c6a9e
Ewerton <eww999@hotmail.com>
2026-04-21 23:33:30 -0300

    chore: add docker, postgres and environment configuration

---
+++ src/test/java/br/ufrn/imd/warehouse/WarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
