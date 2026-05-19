28028b053678c6af00df0f24b56d19c1fd63409c
Ewerton <eww999@hotmail.com>
2026-05-18 21:40:10 -0300

    test:test

--- api/src/main/java/br/ufrn/imd/warehouse/exceptions/AlreadyExistsException.java
+++ api/src/main/java/br/ufrn/imd/warehouse/exceptions/AlreadyExistsException.java
@@ -1,5 +1,5 @@
 package br.ufrn.imd.warehouse.exceptions;
-
+//Comentário teste 2
 public class AlreadyExistsException extends BusinessException {
   public AlreadyExistsException(Object... args) {
     super("already.exists", args);
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,5 +9,5 @@ function Home() {
     </div>
   );
 }
-//comentário de testes
+//comentário de teste1
 export default Home;
