088b044f32951f8c5c66e213b3742d59768b9758
Gabriel Guilherme <gabriel.guilherme.131@ufrn.edu.br>
2026-06-12 18:46:07 -0300

    sem mensagem no dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,10 +3,7 @@ function Dashboard() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-slate-800 mb-4">Dashboard</h1>
-      <p className="text-slate-600">
-        Bem-vindo ao sistema de gerenciamento. Selecione "Gerenciar Estoque" na
-        barra lateral para acessar o CRUD.
-      </p>
+      <p className="text-slate-600"></p>
     </div>
   );
 }
