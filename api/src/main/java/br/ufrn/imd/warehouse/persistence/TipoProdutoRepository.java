fd3945053d28df75aaf8196a80208303c55a9801
Ewerton <eww999@hotmail.com>
2026-06-14 22:26:51 -0300

    feat: adiciona criação de tipo produto

--- api/src/main/java/br/ufrn/imd/warehouse/persistence/TipoProdutoRepository.java
+++ api/src/main/java/br/ufrn/imd/warehouse/persistence/TipoProdutoRepository.java
@@ -1,7 +1,14 @@
 package br.ufrn.imd.warehouse.persistence;
 
+import br.ufrn.imd.warehouse.domain.entities.TipoProduto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TipoProdutoRepository {
+public interface TipoProdutoRepository extends AbstractRepository<TipoProduto> {
+
+    @Query("SELECT t FROM TipoProduto t WHERE t.ativo = true AND t.denominacao = :denominacao")
+    TipoProduto findByDenominacao(@Param("denominacao") String denominacao);
 }
