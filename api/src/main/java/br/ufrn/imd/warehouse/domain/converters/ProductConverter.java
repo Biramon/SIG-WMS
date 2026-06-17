4e2254e518d57f417ca858a0b1ffb566b0a565a2
Ivison Filho <ivisoncaufilho@gmail.com>
2026-06-17 13:03:26 -0300

    refactor: criar DTOs resumidos para evitar recursão entre Product e TipoProduto
    - Cria ProductResumoDto para representações simplificadas
    - Altera TipoProdutoDto para usar List<ProductResumoDto>
    - Remove referência circular entre ProductDto e TipoProdutoDto
    - Ajusta mapeamentos do MapStruct para utilizar DTOs resumidos
    - Evita recursão infinita durante conversão e serialização

--- api/src/main/java/br/ufrn/imd/warehouse/domain/converters/ProductConverter.java
+++ api/src/main/java/br/ufrn/imd/warehouse/domain/converters/ProductConverter.java
@@ -1,7 +1,7 @@
 package br.ufrn.imd.warehouse.domain.converters;
 
 import br.ufrn.imd.warehouse.domain.dtos.ProductDto;
-
+import br.ufrn.imd.warehouse.domain.dtos.ProductResumoDto;
 import br.ufrn.imd.warehouse.domain.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -15,7 +15,9 @@ public interface ProductConverter {
     Product toEntity(ProductDto dto);
 
     @Mapping(target = "unidadeMedida", qualifiedByName = "toDtoUnidadeMedida")
-    ProductDto toDto(Product product);
+    ProductDto toDto(Product entity);
 
-    List<ProductDto> toListDto(List<Product> products);
+    ProductResumoDto toResumoDto(Product entity);
+
+    List<ProductDto> toListDto(List<Product> entities);
 }
