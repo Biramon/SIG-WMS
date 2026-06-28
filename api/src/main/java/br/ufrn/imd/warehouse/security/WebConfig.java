bb0c479769b85cd04e92b7af2f8a19bc9f4a9b9a
Ewerton <eww999@hotmail.com>
2026-06-28 20:37:38 -0300

    refactor: conserta bug no cors

--- api/src/main/java/br/ufrn/imd/warehouse/security/WebConfig.java
+++ api/src/main/java/br/ufrn/imd/warehouse/security/WebConfig.java
@@ -1,11 +1,14 @@
 package br.ufrn.imd.warehouse.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("!test")
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
