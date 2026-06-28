aab0748dfadf9c2bd0265e0a264eabdd1e99db51
Ewerton <eww999@hotmail.com>
2026-06-28 20:16:27 -0300

    feat: adiciona o cors

---
+++ api/src/main/java/br/ufrn/imd/warehouse/security/WebConfig.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd.warehouse.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://sig-wms-frontend.onrender.com", "http://localhost:5174")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
