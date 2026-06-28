1114d2907eab61b07d24c97b0fad6ad012838472
Ewerton <eww999@hotmail.com>
2026-06-28 20:46:49 -0300

    refactor: conserta bug no cors

--- api/src/main/java/br/ufrn/imd/warehouse/config/WebConfig.java
+++ api/src/main/java/br/ufrn/imd/warehouse/config/WebConfig.java
@@ -1,18 +1,20 @@
 package br.ufrn.imd.warehouse.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("!test")
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Libera todas as rotas da API (products, tipo-produto, etc)
-                .allowedOrigins("http://localhost:5174") // URL exata do seu servidor de dev do React
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD") // Métodos HTTP permitidos
-                .allowedHeaders("*") // Permite qualquer cabeçalho na requisição
-                .allowCredentials(true); // Permite o envio de cookies/autenticação se precisar no futuro
+        registry.addMapping("/**")
+                .allowedOrigins("https://sig-wms-frontend.onrender.com", "http://localhost:5174")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
